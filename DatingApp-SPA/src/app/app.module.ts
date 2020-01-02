f572b02f9fff33ed887375fcff1d1ff4b203ce21
MMrFalcon <kubasokol123456@gmail.com>
2020-01-02 19:14:45 +0100

    Adding app members as a cards inheriting from parent component

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -11,11 +11,12 @@ import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
+import { MemberCardsComponent } from './members/member-cards/member-cards.component';
 
 @NgModule({
    declarations: [
@@ -25,7 +26,8 @@ import { appRoutes } from './routes';
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardsComponent
    ],
    imports: [
       BrowserModule,
