c0fb91b71c8e7e688dd7b3a487e9699b2cc6f1d7
MMrFalcon <kubasokol123456@gmail.com>
2020-02-17 18:31:47 +0100

    Adding Messages Entity.

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -15,6 +15,7 @@ namespace DatingApp.API.Data
 
         public DbSet<Like> Likes { get; set; }
 
+        public DbSet<Message> Messages { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Like>().HasKey(key => new {key.LikerId, key.LikeeId});
@@ -24,6 +25,14 @@ namespace DatingApp.API.Data
 
             builder.Entity<Like>().HasOne(user => user.Liker).WithMany(user => user.Likees)
                     .HasForeignKey(user => user.LikerId).OnDelete(DeleteBehavior.Restrict);
+
+
+            builder.Entity<Message>().HasOne(user => user.Sender).WithMany(message => message.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict); 
+
+            builder.Entity<Message>().HasOne(user => user.Recipient).WithMany(message => message.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict); 
+            
         }
     }
 }
